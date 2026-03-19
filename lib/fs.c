ae4bae0c0e97aeda612de2edc85217448d504249
Mauricio-100 <ceoseshell@gmail.com>
2026-03-19 16:07:11 +0100

    Update fs.c

--- lib/fs.c
+++ lib/fs.c
@@ -1,47 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../src/gsc.h"
+#include <string.h>
+#include "gsc.h"
 
 /* (fs/read-file "path") */
-static fe_Object* f_fs_read(fe_Context *ctx, fe_Object *args) {
+static gs_Object* f_fs_read(gs_Context *ctx, gs_Object *args) {
   char path[256], *buf;
   long sz;
-  fe_Object *res;
-  fe_tostring(ctx, fe_nextarg(ctx, &args), path, sizeof(path));
+  gs_Object *res;
+  
+  gs_tostring(ctx, gs_nextarg(ctx, &args), path, sizeof(path));
   
   FILE *f = fopen(path, "rb");
-  if (!f) return fe_bool(ctx, 0); // Retourne nil si échec
+  if (!f) return gs_bool(ctx, 0); // Retourne nil si échec
 
   fseek(f, 0, SEEK_END);
   sz = ftell(f);
   fseek(f, 0, SEEK_SET);
   
-  buf = malloc(sz + 1);
+  buf = (char*)malloc(sz + 1);
+  if (!buf) {
+    fclose(f);
+    return gs_bool(ctx, 0);
+  }
+  
   fread(buf, 1, sz, f);
   buf[sz] = '\0';
   fclose(f);
 
-  res = fe_string(ctx, buf);
+  res = gs_string(ctx, buf);
   free(buf);
   return res;
 }
 
 /* (fs/write-file "path" "content") */
-static fe_Object* f_fs_write(fe_Context *ctx, fe_Object *args) {
-  char path[256], content[1024]; // Ajuste la taille si besoin
-  fe_tostring(ctx, fe_nextarg(ctx, &args), path, sizeof(path));
-  fe_tostring(ctx, fe_nextarg(ctx, &args), content, sizeof(content));
+static gs_Object* f_fs_write(gs_Context *ctx, gs_Object *args) {
+  char path[256];
+  char content[1024]; // Ajuste la taille si besoin
+  
+  gs_tostring(ctx, gs_nextarg(ctx, &args), path, sizeof(path));
+  gs_tostring(ctx, gs_nextarg(ctx, &args), content, sizeof(content));
   
   FILE *f = fopen(path, "wb");
-  if (!f) return fe_bool(ctx, 0);
+  if (!f) return gs_bool(ctx, 0);
   
   fprintf(f, "%s", content);
   fclose(f);
-  return fe_bool(ctx, 1); // Retourne t
+  return gs_bool(ctx, 1); // Retourne t
 }
 
 /* Enregistrement des fonctions */
-void fs_init(fe_Context *ctx) {
-  fe_set(ctx, fe_symbol(ctx, "fs/read-file"), fe_cfunc(ctx, f_fs_read));
-  fe_set(ctx, fe_symbol(ctx, "fs/write-file"), fe_cfunc(ctx, f_fs_write));
+void fs_init(gs_Context *ctx) {
+  gs_set(ctx, gs_symbol(ctx, "fs/read-file"), gs_cfunc(ctx, f_fs_read));
+  gs_set(ctx, gs_symbol(ctx, "fs/write-file"), gs_cfunc(ctx, f_fs_write));
 }
