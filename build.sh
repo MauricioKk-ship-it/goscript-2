fde53d89dfd5fa768ace14d4c806ae0c619127e3
Mauricio-100 <ceoseshell@gmail.com>
2026-03-19 16:11:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-gcc src/module.c src/fs.c src/gsc.c -DFE_STANDALONE -O3 -o gd -Wall -Wextra -std=c99 -pedantic
+
+gcc -DGS_STANDALONE \
+    -I./src \
+    -c src/gsc.c -o build/gsc.o
+
+gcc -DGS_STANDALONE \
+    -I./src \
+    -c src/module.c -o build/module.o
+
+gcc -DGS_STANDALONE \
+    -I./src \
+    -c lib/fs.c -o build/fs.o
+
+gcc -DGS_STANDALONE \
+    build/gsc.o build/module.o build/fs.o \
+    -o goscript -lm
+
+echo "Compilation terminée: ./goscript"
