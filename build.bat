3d5664fbefe98f95fce4c1416b12d8526998d190
Your Name <you@example.com>
2026-03-20 08:21:26 +0000

    @

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem download this:
-rem https://nuwen.net/mingw.html
-
-gcc src/gsc.c src/module.c -DFE_STANDALONE -O3 -o fe -Wall -Wextra -std=c99 -pedantic
-strip gd.exe
--- fis.gjs
+++
@@ -1,6 +0,0 @@
-fn calculer(a, b) {
-  let res = +(a, b);
-  print("Le résultat est:", res);
-}
-
-calculer(10, 20);
